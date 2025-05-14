02da02c5499c6dd2a73a08e22e2050fed0019e09
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-05-14 15:30:07 -0300

    chore: eslint rules

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+    ignores: ['package-lock.json'],
+  },
+  { ignores: ['src/infra/config/db/prisma/generated', './dist'] },
+]);
